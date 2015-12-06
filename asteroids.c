97373f0a00d38f9067a124d217b91cf417b87369
ResPasha <respasha@yandex.ru>
2015-12-06 09:46:56 +0300

    Ship flies
    Initialisation, input processing, world updating and rendering separated
    Architecture should be OK

--- asteroids.c
+++ asteroids.c
@@ -20,17 +20,15 @@ typedef struct {
 
 void cleanup()
 {
-	printf("Quiting SDL.\n");
 	SDL_Quit();
-    printf("Quiting....\n");
 	exit(0);
 }
 
-void putpixel(SDL_Surface *surface, pixel_t* pixel, uint32_t color)
+void putpixel(SDL_Surface *screen, pixel_t* pixel, uint32_t color)
 {
-    int bpp = surface->format->BytesPerPixel;
+    int bpp = screen->format->BytesPerPixel;
     /* Here p is the address to the pixel we want to set */
-    uint8_t *p = (uint8_t *) surface -> pixels + pixel -> y * surface -> pitch + pixel -> x * bpp;	
+    uint8_t *p = (uint8_t *) screen -> pixels + pixel -> y * screen -> pitch + pixel -> x * bpp;	
 	if (pixel -> y < 0)
 		return;
 	if (pixel -> x < 0)
@@ -75,42 +73,72 @@ pixel_t* pixel_create(unsigned int const x,
 	return pixel;
 }
 
-void line(SDL_Surface* surface, pixel_t* const from, pixel_t* const to, uint32_t color)
+void swap(int* a, int* b) 
 {
+	int r = *a;
+	*a = *b;
+	*b = r;
+}
+
+void line(SDL_Surface* surface, pixel_t* const from, pixel_t* const to, uint32_t color)
+{	
 	double dy = (to -> y) - (from -> y);
 	double dx = (to -> x) - (from -> x);
-	if (dx >= dy)
+	if (abs(dx) >= abs(dy))
 	{
 		int x;
+		int a = from -> x;
+		int b = to -> x;
 		double k = dy / dx;
-		double b = from -> y + 0.5 - k * (from -> x);
-		for (x = from -> x; x <= to -> x; x++)
+		double c = from -> y + 0.5 - k * (from -> x);
+		if (b < a) swap(&a, &b);
+		for (x = a; x <= b; x++)
 		{
-			int y = (int) floor( k * x + b );
+			int y = (int) floor( k * x + c );
 			putpixel(surface, pixel_create(x, y), color);
 		}
 	}
 	else
 	{
 		int y;
+		int a = from -> y;
+		int b = to -> y;
 		double k = dx / dy;
-		double b = from -> x + 0.5 - k * (from -> y);
-		for (y = from -> y; y <= to -> y; y++)
+		double c = from -> x + 0.5 - k * (from -> y);
+		if (b < a) swap(&a, &b);
+		for (y = a; y <= b; y++)
 		{
-			int x = (int) floor( k * y + b );
+			int x = (int) floor( k * y + c );
 			putpixel(surface, pixel_create(x, y), color);
 		}
 	}
 }
 
-void ship_draw(SDL_Surface* screen, ship_t* ship)
+pixel_t* pixel_create_from(pixel_t* o, int dx, int dy, double angle)
 {
+	int x = (int) floor(dx * cos(angle) + dy * sin(angle));
+	int y = (int) floor( -1 * dx * sin(angle) + dy * cos(angle));
+	x += o -> x;
+	y += o -> y;
+	return pixel_create(x, y);
+}
+
+void ship_draw(SDL_Surface* screen, ship_t* ship)
+{ /* ДАВАЙ РИСОВАТЬ КОРАБЛИК! */
 	pixel_t* center = ship -> place;
-	int x = center -> x;
-	int y = center -> y;
+	double angle = ship -> angle;
 	uint32_t color = SDL_MapRGB(screen->format, 0xff, 0xff, 0xff);
-	line(screen, pixel_create(x - 9, y - 5), pixel_create(x + 9, y + 5), color);
-	line(screen, pixel_create(x - 5, y + 5), pixel_create(x + 5, y - 5), color);
+	pixel_t* front = pixel_create_from(center, 20, 0, angle);
+	pixel_t* right = pixel_create_from(center, 0, 8, angle);
+	pixel_t* right_back = pixel_create_from(center, -4, 10, angle);
+	pixel_t* left = pixel_create_from(center, 0, -8, angle);
+	pixel_t* left_back = pixel_create_from(center, -4, -10, angle);
+	line(screen, front, right_back, color);
+	line(screen, front, left_back, color);
+	line(screen, left, right, color);
+	free(front);
+	free(right); free(right_back);
+	free(left); free(left_back);
 }
 
 void draw(SDL_Surface* screen, ship_t* ship)
@@ -133,20 +161,16 @@ void draw(SDL_Surface* screen, ship_t* ship)
 	SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
 
-int main (int argc, char** argv)
+SDL_Surface* init_video()
 {
-	SDL_Surface *screen;
-	ship_t* ship;
+	SDL_Surface* screen;
 	
-	
-    printf("Initializing SDL.\n");
 	/* Initialize defaults, Video and Audio */
     if((SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)==-1)) { 
         fprintf(stderr, "Could not initialize SDL: %s.\n",
 						SDL_GetError());
         exit(-1);
     }
-    printf("SDL initialized.\n");
 	
 	atexit(cleanup); /*cleanup on exit*/
 	
@@ -158,125 +182,130 @@ int main (int argc, char** argv)
     }
 	SDL_WM_SetCaption("asteroids", NULL);
 	
-	ship  = (ship_t*) malloc(sizeof(ship_t));
+	return screen;
+}
+
+ship_t* init_world()
+{	
+	ship_t* ship  = (ship_t*) malloc(sizeof(ship_t));
 	ship -> place = pixel_create(70, 70);
 	ship -> speed_x = 0;
 	ship -> speed_y = 0;
-	
-	
-	while(1)
-	{
-		SDL_Event event;
-		
-		draw(screen, ship);
-		
-		if (ship -> engine)
-		{
-			double accel = 3;
-			ship -> speed_x += accel * cos(ship -> angle);
-			ship -> speed_y += accel * sin(ship -> angle);
-		}
-		else
-		{
-			double accel = 0.999;
-			ship -> speed_x *= accel;
-			ship -> speed_y *= accel;
-		}
-		
-		ship -> place -> x += (int) floor(ship -> speed_x + 0.5);
-		ship -> place -> y += (int) floor(ship -> speed_y + 0.5);
-		
-		{
-			Uint8* keystate = SDL_GetKeyState(NULL);
-			if(keystate[SDLK_LEFT])
-			{
-			}
-			if(keystate[SDLK_RIGHT])
-			{
-			}
-			if(keystate[SDLK_UP])
-			{
-			}
-			
-			while(SDL_PollEvent(&event))
-			{  
-				switch(event.type)
-				{  				
-					case SDL_KEYDOWN:
-					/* handle keyboard stuff here */
-						switch (event.key.keysym.sym)
-						{
-							case SDLK_ESCAPE:
-								exit(0);
-							break;
-							
-							case SDLK_LEFT:
-								puts("<");
-								ship -> speed_x --;
-							break;
-							
-							case SDLK_RIGHT:
-								puts(">");
-								ship -> speed_x ++;
-							break;
-							
-							case SDLK_UP:
-								puts("^");
-								ship -> speed_y --;
-							break;
-							
-							case SDLK_DOWN:
-								puts("V");
-								ship -> speed_y ++;
-							break;
-							
-							default:
-							break;
-						}				
-					break;
-					
-					case SDL_KEYUP:
-						switch (event.key.keysym.sym)
-						{
-							
-						}
-					break;
+	ship -> rotating_left = 0;
+	ship -> rotating_right = 0;
+	ship -> engine = 0;
+	ship -> angle = 0;
+	return ship;
+}
 
-					case SDL_QUIT:
-					/* Set whatever flags are necessary to */
-					/* end the main game loop here */
+void process_input(ship_t* ship)
+{
+	SDL_Event event;	
+	while(SDL_PollEvent(&event))
+	{  
+		switch(event.type)
+		{  				
+			case SDL_KEYDOWN:
+			/* handle keyboard stuff here */
+				switch (event.key.keysym.sym)
+				{
+					case SDLK_ESCAPE:
 						exit(0);
 					break;
-				}
-			}
-		}
+					
+					case SDLK_LEFT:
+						ship -> rotating_left = 1;
+					break;
+					
+					case SDLK_RIGHT:
+						ship ->rotating_right = 1;
+					break;
+					
+					case SDLK_UP:
+						ship -> engine = 1;
+					break;
+					
+					case SDLK_SPACE:
+						ship -> shoot = 1;
+					break;
+					
+					default:
+					break;
+				}				
+			break;
 		
-		SDL_Delay(16);
+			case SDL_KEYUP:
+				switch (event.key.keysym.sym)
+				{
+					case SDLK_LEFT:
+						ship -> rotating_left = 0;
+					break;
+					
+					case SDLK_RIGHT:
+						ship -> rotating_right = 0;
+					break;
+					
+					case SDLK_UP:
+						ship -> engine = 0;
+					break;
+					
+					case SDLK_SPACE:
+						ship -> shoot = 0;
+					break;
+					
+					default:
+					break;
+				}
+			break;
+
+			case SDL_QUIT:
+			/* Set whatever flags are necessary to */
+			/* end the main game loop here */
+				exit(0);
+			break;
+		}
 	}
 }
 
-int newmain(int argc, char** argv)
+void update_world(ship_t* ship)
 {
-	int done = 0;
-	double timeStepMs;
-	
-	init();
-	timeStepMs = 1000.f / 60;
-	init_world();
-	
-	while (!done)
+	double degree = 0.05; 
+	if (ship -> rotating_left) 
 	{
-		timeLastMs = timeCurrentMs;
-		timeCurrentMs = SDL_GetTicks();
-		timeDeltaMs = timeCurrentMs - timeLastMs;
-		timeAccMs += timeDeltaMs;
+		ship -> angle += degree;
+	}
+	if (ship -> rotating_right)
+	{
+		ship -> angle -= degree;
+	}
+	
+	if (ship -> engine)
+	{
+		double accel = 0.1;
+		ship -> speed_x += accel * cos(ship -> angle);
+		ship -> speed_y -= accel * sin(ship -> angle);
+	}
+	else
+	{
+		double accel = 0.995;
+		ship -> speed_x *= accel;
+		ship -> speed_y *= accel;
+	}
 		
-		while (timeAccMs >= timeStepMs)
-		{
-			processInput();
-			updateWorld();
-			timeAccMs -= timeStepMs;
-		}
-		render();
+	ship -> place -> x += (int) floor(ship -> speed_x + 0.5);
+	ship -> place -> y += (int) floor(ship -> speed_y + 0.5);
+}
+
+int main (int argc, char** argv)
+{
+	SDL_Surface *screen = init_video();
+	ship_t* ship = init_world();
+	
+	while(1)
+	{		
+		process_input(ship);
+		update_world(ship);
+		draw(screen, ship); /*render*/
+		SDL_Delay(16);
 	}
 }
--- makefile
+++ makefile
@@ -4,7 +4,7 @@ LFLAGS = `sdl-config --libs` -lSDL
 all: main
 
 main:
-	gcc asteroids.c -o asteroids $(CFLAGS) $(LFLAGS)
+	gcc asteroids.c -o asteroids.out $(CFLAGS) $(LFLAGS)
 	
 clean:
 	rm asteroids
