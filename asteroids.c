c0606171cbf4e7e4a650cf74442366eddf344a1d
ResPasha <respasha@yandex.ru>
2016-01-05 14:13:09 +0300

    Missles added

--- asteroids.c
+++ asteroids.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 #include <math.h>
 
+#define EPS 0.000001
+#define WIDTH 640
+#define HEIGHT 480
+
+#define SHIP_MAX_SPEED 20
+#define MISSLE_SPEED 13
+#define MISSLE_LIFE 30
+
 typedef struct {
 	int x;
 	int y;
 } pixel_t;
 
 typedef struct {
-	pixel_t* place;
+	double x;
+	double y;
 	double angle;
 	int engine;
 	int rotating_right;
@@ -17,6 +26,38 @@ typedef struct {
 	double speed_x;
 	double speed_y;
 } ship_t;
+
+typedef struct missle_t {
+	double x;
+	double y;
+	double speed_x;
+	double speed_y;	
+	int life;
+	struct missle_t* next;
+} missle_t;
+
+typedef struct asteroid_t {
+	double x;
+	double y;
+	double speed_x;
+	double speed_y;
+	int size;
+	struct asteroid_t* next;
+} asteroid_t;
+
+typedef struct {
+	double x;
+	double y;
+	double speed_x;
+	double speed_y;	
+} enemy_t;
+
+typedef struct {
+	ship_t* ship;
+	missle_t* missles;
+	asteroid_t* asteroids;
+	enemy_t* enemies;
+} world_t;
 
 void cleanup()
 {
@@ -123,9 +164,9 @@ pixel_t* pixel_create_from(pixel_t* o, int dx, int dy, double angle)
 	return pixel_create(x, y);
 }
 
-void ship_draw(SDL_Surface* screen, ship_t* ship)
+void draw_ship(SDL_Surface* screen, ship_t* ship)
 { /* ДАВАЙ РИСОВАТЬ КОРАБЛИК! */
-	pixel_t* center = ship -> place;
+	pixel_t* center = pixel_create((int)round(ship->x),(int)round(ship->y));
 	double angle = ship -> angle;
 	uint32_t color = SDL_MapRGB(screen->format, 0xff, 0xff, 0xff);
 	pixel_t* front = pixel_create_from(center, 20, 0, angle);
@@ -141,42 +182,55 @@ void ship_draw(SDL_Surface* screen, ship_t* ship)
 	free(left); free(left_back);
 }
 
-void draw(SDL_Surface* screen, ship_t* ship)
+void draw_asts(SDL_Surface* screen, asteroid_t* list)
 {
 	
+}
+
+void draw_missles(SDL_Surface* screen, missle_t* list)
+{
+	uint32_t color = SDL_MapRGB(screen->format, 0xff, 0xff, 0x00);
+	while (list != NULL)
+	{
+		pixel_t* pixel = pixel_create((int)list -> x, (int)list -> y);
+		putpixel(screen, pixel, color);
+		free(pixel);
+		list = list -> next;
+	}
+}
+
+void draw(SDL_Surface* screen, world_t* world)
+{
 	if ( SDL_MUSTLOCK(screen) ) {
 		if ( SDL_LockSurface(screen) < 0 ) {
 			fprintf(stderr, "Can't lock screen: %s\n", SDL_GetError());
 		}
 	}
-	
 	SDL_FillRect(screen, NULL, 0x000000);
 	
-	ship_draw(screen, ship);
+	draw_ship(screen, world -> ship);
+	draw_asts(screen, world -> asteroids);
+	draw_missles(screen, world -> missles);
 	
 	if ( SDL_MUSTLOCK(screen) ) {
 		SDL_UnlockSurface(screen);
 	}
-	
 	SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
 
 SDL_Surface* init_video()
 {
 	SDL_Surface* screen;
-	
-	/* Initialize defaults, Video and Audio */
     if((SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)==-1)) { 
         fprintf(stderr, "Could not initialize SDL: %s.\n",
 						SDL_GetError());
         exit(-1);
     }
-	
-	atexit(cleanup); /*cleanup on exit*/
-	
-	screen = SDL_SetVideoMode(640, 480, 8, SDL_SWSURFACE);
+	atexit(cleanup); 
+	screen = SDL_SetVideoMode(WIDTH, HEIGHT, 8, SDL_SWSURFACE);
     if ( screen == NULL ) {
-        fprintf(stderr, "Couldn't set 640x480x8 video mode: %s\n",
+        fprintf(stderr, "Couldn't set %dx%dx8 video mode: %s\n",
+						WIDTH, HEIGHT,
                         SDL_GetError());
         exit(1);
     }
@@ -185,10 +239,11 @@ SDL_Surface* init_video()
 	return screen;
 }
 
-ship_t* init_world()
+ship_t* init_ship()
 {	
 	ship_t* ship  = (ship_t*) malloc(sizeof(ship_t));
-	ship -> place = pixel_create(70, 70);
+	ship -> x = 70;
+	ship -> y = 70;
 	ship -> speed_x = 0;
 	ship -> speed_y = 0;
 	ship -> rotating_left = 0;
@@ -196,6 +251,21 @@ ship_t* init_world()
 	ship -> engine = 0;
 	ship -> angle = 0;
 	return ship;
+}
+
+asteroid_t* init_asteroids()
+{
+	return NULL;
+}
+
+world_t* init_world()
+{
+	world_t* world = (world_t*) malloc(sizeof(world_t));
+	world -> ship = init_ship();
+	world -> asteroids = init_asteroids();
+	world -> missles = NULL;
+	world -> enemies = NULL;
+	return world;
 }
 
 void process_input(ship_t* ship)
@@ -249,10 +319,6 @@ void process_input(ship_t* ship)
 						ship -> engine = 0;
 					break;
 					
-					case SDLK_SPACE:
-						ship -> shoot = 0;
-					break;
-					
 					default:
 					break;
 				}
@@ -267,9 +333,14 @@ void process_input(ship_t* ship)
 	}
 }
 
-void update_world(ship_t* ship)
+double sqr(double x)
 {
-	double degree = 0.05; 
+	return x*x;
+}
+
+void update_ship(ship_t* ship)
+{
+	double degree = 0.08; 
 	if (ship -> rotating_left) 
 	{
 		ship -> angle += degree;
@@ -281,31 +352,112 @@ void update_world(ship_t* ship)
 	
 	if (ship -> engine)
 	{
-		double accel = 0.1;
-		ship -> speed_x += accel * cos(ship -> angle);
-		ship -> speed_y -= accel * sin(ship -> angle);
+		if ( (sqr(ship -> speed_x) + sqr(ship -> speed_y)) < SHIP_MAX_SPEED ) 
+		{
+			double accel = 0.075;
+			ship -> speed_x += accel * cos(ship -> angle);
+			ship -> speed_y -= accel * sin(ship -> angle);
+		}
 	}
 	else
 	{
 		double accel = 0.995;
 		ship -> speed_x *= accel;
 		ship -> speed_y *= accel;
-	}
+	}	
 		
-	ship -> place -> x += (int) floor(ship -> speed_x + 0.5);
-	ship -> place -> y += (int) floor(ship -> speed_y + 0.5);
+	ship -> x += ship -> speed_x;
+	ship -> y += ship -> speed_y;
+	if (ship -> x > WIDTH) ship -> x -= WIDTH;
+	if (ship -> x < 0) ship -> x += WIDTH;
+	if (ship -> y > HEIGHT) ship -> y -= HEIGHT;
+	if (ship -> y < 0) ship -> y += HEIGHT;
+}
+
+asteroid_t* update_asts(asteroid_t* list) {
+	asteroid_t* ret = list;
+	asteroid_t* ast = list;
+	while (ast != NULL)
+	{
+		ast -> x += ast -> speed_x;
+		ast -> y += ast -> speed_y;
+		ast = ast -> next;
+	}
+	return ret;
+}
+
+missle_t* add_missle(missle_t* list, ship_t* ship) 
+{
+	missle_t* missle;
+	double _cos = cos(ship -> angle);
+	double _sin = sin(ship -> angle);
+	if (!(ship -> shoot)) { return list; }
+	missle = (missle_t*)malloc(sizeof(missle_t));
+	missle -> x = 20 * _cos + ship -> x;
+	missle -> y = -20 * _sin + ship -> y;
+	missle -> speed_x = MISSLE_SPEED * _cos;
+	missle -> speed_y = MISSLE_SPEED * _sin * (-1);
+	missle -> life = MISSLE_LIFE;
+	ship -> shoot = 0;
+	missle -> next = list;
+	return missle;
+}
+
+missle_t* update_missles(missle_t* list) {
+	missle_t* ret = list;
+	missle_t* missle = list;
+	missle_t* prev = NULL;
+	while (missle != NULL)
+	{
+		missle -> x += missle -> speed_x;
+		missle -> y += missle -> speed_y;
+		missle -> life -= 1;
+		if (missle -> life == 0)
+		{
+			if (prev == NULL)
+			{
+				ret = missle -> next;
+				free(missle);
+				missle = ret;
+			}
+			else
+			{
+				prev -> next = missle -> next;
+				free(missle);
+				missle = prev -> next;
+			}
+			continue;
+		}
+		prev = missle;
+		missle = missle -> next;
+	}
+	return ret;
+}
+
+void detect_collissions(world_t* world)
+{
+	return;
+}
+
+void update_world(world_t* world)
+{
+	update_ship(world -> ship);
+	world -> missles = add_missle(world -> missles, world -> ship);
+	world -> missles = update_missles(world -> missles);
+	world -> asteroids = update_asts(world -> asteroids);
+	detect_collissions(world);
 }
 
 int main (int argc, char** argv)
 {
 	SDL_Surface *screen = init_video();
-	ship_t* ship = init_world();
+	world_t* world = init_world();
 	
 	while(1)
 	{		
-		process_input(ship);
-		update_world(ship);
-		draw(screen, ship); /*render*/
+		process_input(world -> ship);
+		update_world(world);
+		draw(screen, world); /*render*/
 		SDL_Delay(16);
 	}
 }
